8761795431cf4f6254b93df31a8d0c930f3f0ef6
Stephen Gouws <development@sgouws.com>
2017-06-02 12:47:26 +0200

    folder reorder

---
+++ page-home.php
@@ -0,0 +1,41 @@
+<?php
+
+get_header();
+?>
+<div id="main-image" class="banner-img"></div>
+<div class="container">
+	<div class="lettering-logo"><img src="http://localhost/www/epsilon/wp-content/uploads/2017/06/Untitled-6-2.png"></div>
+</div>
+<div class="content">
+	<div class="container">
+		<div class="learn-more-section clearfix">
+			<span class="learn-more-text">Ad quis audire pri, ius at aperiri oblique</span>
+			<span class="learn-more-button">Learn More</span></div>
+		<div class="header-text-section">
+			Usu ignota doming appareat cu. Id eum tale oratio. An qui ubique fabellas, legimus eligendi ex quo. At paulo laoreet pri, in ubique accusata adipiscing per, no meis ocurreret mei.
+		</div>
+
+		<div class="image-section">
+			<div class="image-overlay">
+				<div class="image-overlay-heading">
+					Ad quis audire pri, ius at aperiri oblique.</div>
+				<hr/>
+				<div class="image-overlay-subtext">
+					Photography</div>
+			</div>
+			<img src="http://localhost/www/epsilon/wp-content/uploads/2017/06/man-old-bike-nature-50546.jpeg">
+			<div class="image-overlay">
+				<div class="image-overlay-heading">
+					Ad quis audire pri, ius at aperiri oblique.</div>
+				<hr/>
+				<div class="image-overlay-subtext">
+					Photography</div>
+			</div>
+			<div class="image-section"><img src="http://localhost/www/epsilon/wp-content/uploads/2017/06/pexels-photo-407403.jpeg"></div>
+		</div>
+	</div>
+</div>
+<?php
+get_footer();
+
+?>
